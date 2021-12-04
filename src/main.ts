82977ff24fcd318aa71201a97fb7d1c65862f0fa
jbb <c799297625@126.com>
2021-12-04 10:35:31 +0800

    test: 'test'

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
 import { createPinia } from 'pinia'
-import 'vue-global-api'
 import naive from 'naive-ui'
+import 'vue-global-api'
 
 const app = createApp(App)
 app.use(router)
